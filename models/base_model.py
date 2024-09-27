f2611a1ad1a8cb309bca41ecf7e8866bb8764abd
chepeniv <jose.olmos@atlasschool.com>
2024-09-27 18:53:22 -0500

    adding __str__ and __init__ methods as well as setters

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
 
-class Base: 
 
-    @attribute
+from uuid import uuid4
+
+class BaseModel: 
+
+    def __init__(self):
+        self.id = str(uuid4())
+        print(self.id)
+
+    def __str__(self):
+        """ [<class name>] (<self.id>) <self.__dict__>
+        """
+        obj_str = "[{}] ({}) {}"
+        return obj_str.format(type(self), self.id, self.__dict__)
+
+    @property
     def id(self):
         return self.__id
 
-    @attribute
+    @property
     def created_at(self):
         return self.__created_at
 
-    @attribute
+    @property
     def updated_at(self):
         return self.__updated_at
+
+    @id.setter
+    def id(self, new):
+        self.__id = new
+
+    @created_at.setter
+    def created_at(self, new):
+        self.__created_at = new
+
+    @created_at.setter
+    def updated_at(self, new):
+        self.__updated_at = new
 
     def save(self):
         pass
