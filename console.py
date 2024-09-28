e1cce49003f8a026d5513ee211357cfd52e87df5
chepeniv <jose.olmos@atlasschool.com>
2024-09-28 18:57:37 -0500

    checker testing task 5

--- console.py
+++ console.py
@@ -8,21 +8,31 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """ our reimplementation of cmd.Cmd
     """
-    # update help command
-    # code should not execute one import
     prompt = '(hbnb) '
     file = None
 
-    def close(self):
-        if self.file:
-            self.file.close()
-            self.file = None
-    
+    # def close(self):
+    #     if self.file:
+    #         self.file.close()
+    #         self.file = None
+
     def do_quit(self, arg):
         'exit this CLI instance hbnb'
-        self.close()
+        # self.close()
         quit()
+"""
+    def do_create(self, arg):
+        'creates a new instance of BaseModel'
+        # save to json_file
+        # print id
+        # if class name is missing print: ** class name missing **
+        # if class name doesnt exist print: ** class doesn't exist **
+        pass
 
+    def do_show(self, arg):
+        'prints string representation of given object'
+        pass
+"""
     def emptyline(self):
         pass
     
