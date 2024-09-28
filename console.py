179a2459dd01465ca132067981d496ce03c02133
chepeniv <jose.olmos@atlasschool.com>
2024-09-28 18:25:25 -0500

    added do_quit() and close() as well as vars prompt and file

--- console.py
+++ console.py
@@ -3,12 +3,21 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    # implement quit and EOF
+    # implement EOF
     # update help command
-    # use custom prom (hbnb)
-    # empty-line ENTER should do nothing
     # code should not execute one import
-    pass
+    prompt = '(hbnb) '
+    file = None
+
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+    
+    def do_quit(self, arg):
+        'exit this CLI instance hbnb'
+        self.close()
+        quit()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
