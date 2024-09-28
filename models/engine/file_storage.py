f35efa627910bd59ce8ada5685720565cb77a2d0
chepeniv <jose.olmos@atlasschool.com>
2024-09-28 05:21:12 -0500

    moved private attributes to init()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,11 +5,10 @@ import json
 
 class FileStorage:
 
-    __file_path = "file.json"
-    __objects = {}
 
     def __init__(self):
-        pass
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
         return self.__objects
