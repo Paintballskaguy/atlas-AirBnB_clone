61040b3b7e874d2ea985c161c57aa6c7f1daa334
Paintballskaguy <Johnstournament@gmail.com>
2024-09-28 17:52:54 -0500

    reverting back to original

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
+
 import json
-from os import path
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file back to instances.
-    """
+
 
     def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+        self.file_path = "file.json"
+        self.objects = {}
 
     @property
     def file_path(self):
@@ -29,39 +27,25 @@ class FileStorage:
         self.__file_path = new
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
-        return self.__objects
+        return self.objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id.
-        """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj.to_dict()  
-        
+        key = type(obj).__name__ 
+        key = key + obj.id
+        self.objects.update({key: str(obj) })
+ 
     def save(self):
-        """
-        Serializes __objects to the JSON file (path: __file_path).
-        """
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as json_file:
-            json.dump(obj_dict, json_file)
-
+        json_string = json.dumps(self.objects)
+        try: 
+            json_file = open(self.file_path, "w") 
+            json_file.write(json_string)
+        except FileNotFoundError:
+            pass
+  
     def reload(self):
-        """
-        Deserializes the JSON file to __objects (if the file exists).
-        If the file doesn’t exist, do nothing.
-        """
-        if path.exists(self.__file_path):
-            try:
-                with open(self.__file_path, 'r') as json_file:
-                    obj_dict = json.load(json_file)
-                    from models.base_model import BaseModel
-                    for key, value in obj_dict.items():
-                        cls_name = value['__class__']
-                        if cls_name == "BaseModel":
-                            self.__objects[key] = BaseModel(**value)  # Correctly store BaseModel instance
-            except FileNotFoundError:
-                pass
+        try: 
+            json_file = open(self.file_path, "r") 
+            json_string = json_file.read()
+            self.objects = json.loads(json_string)
+        except FileNotFoundError:
+            self.objects = {}
