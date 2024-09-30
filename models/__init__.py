3f5cf551c5533f7e01a170d73835a860e37519d7
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 06:20:17 -0500

    forgot to add commas

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,12 @@
 import models.engine.file_storage as fs
 
 model_classes = {
-        'BaseModel': models.base_model.BaseModel
-        'User': models.user.User
-        'Amenity': models.amenity.Amenity
-        'City': models.city.City
-        'Place': models.place.Place
-        'State': models.state.State
+        'BaseModel': models.base_model.BaseModel,
+        'User': models.user.User,
+        'Amenity': models.amenity.Amenity,
+        'City': models.city.City,
+        'Place': models.place.Place,
+        'State': models.state.State,
         'Review': models.review.Review
         }
 
