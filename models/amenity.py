fe5ee18c34b2bee003fa7efdb321f2d6e19e551c
Paintballskaguy <Johnstournament@gmail.com>
2024-09-29 21:48:28 -0500

    updated city, place, review, amenity, and state.

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Amenity class that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel
+    Public class attributes:
+        name: string - empty string
+    """
+    name = ""
