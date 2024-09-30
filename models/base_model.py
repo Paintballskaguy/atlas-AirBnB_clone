7e8ab23494f3ee214a9b431106b54f0e2a80ba1b
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 04:42:59 -0500

    removed .copy() on __dict__ in BaseModel.to_dict()

--- models/base_model.py
+++ models/base_model.py
@@ -33,12 +33,11 @@ class BaseModel:
         return obj_str.format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        key = models.storage.construct_key(self)
-        models.storage.all().get(key).updated_at = datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
+        obj_dict = self.__dict__
         obj_dict.update({
             '__class__': self.__class__.__name__,
             'id': self.id,
