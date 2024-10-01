9b815a14e40dad1774a8ee0f903db0b1ecaf3a36
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 20:08:39 -0500

    removed setup def

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -6,21 +6,19 @@ from models.place import Place
 
 class TestPlace(unittest.TestCase):
 
-    def setUp(self):
-        self.place = Place()
-
     def test_place__init__(self):
-        self.assertEqual(self.place.name, "")
-        self.assertEqual(self.place.city_id, "")
-        self.assertEqual(self.place.user_id, "")
-        self.assertEqual(self.place.description, "")
-        self.assertEqual(self.place.number_rooms, 0)
-        self.assertEqual(self.place.number_bathrooms, 0)
-        self.assertEqual(self.place.max_guest, 0)
-        self.assertEqual(self.place.price_by_night, 0)
-        self.assertEqual(self.place.latitude, 0.0)
-        self.assertEqual(self.place.longitude, 0.0)
-        self.assertEqual(self.place.anemity_ids, [])
+        place = Place()
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.anemity_ids, [])
 
 if __name__ == "__main__":
     unittest.main()
