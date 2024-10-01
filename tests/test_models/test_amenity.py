8b96986e2492a4378dcc0650edf8eb1336a7df32
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 19:50:27 -0500

    removed class setup and teardown defs

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,19 +2,22 @@
 
 import unittest, os
 from models.amenity import Amenity
-from models.engine.file_storage import FileStorage
+# from models.engine.file_storage import FileStorage
 
 
 class TestAmenity(unittest.TestCase):
 
+    """
     @classmethod
     def setUpClass(cls):
         cls.storage = FileStorage()
+        cls.test_file = "file.json"
 
     @classmethod
     def tearDownClass(cls):
         if os.path.exists(cls.test_file):
             os.remove(cls.test_file)
+    """
 
     def test_amenity__init__(self):
         new_amenity = Amenitiy()
