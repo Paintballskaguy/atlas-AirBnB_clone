42965f06ce556a29202d21d9fb6fd06a52bba2a4
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 19:59:43 -0500

    corrected ref to new state object

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -7,7 +7,7 @@ class TestState(unittest.TestCase):
 
     def test_state__init__(self):
         new_state = State()
-        self.assertEqual(self.state.name, "")
+        self.assertEqual(new_state.name, "")
 
 if __name__ == "__main__":
     unittest.main()
