7aa7ad3265d37fa5ef0cee0da90415389b80979a
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 19:52:58 -0500

    removed commmented out code

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 
-import unittest, os
+import unittest
 from models.amenity import Amenity
-# from models.engine.file_storage import FileStorage
 
 
 class TestAmenity(unittest.TestCase):
-
-    """
-    @classmethod
-    def setUpClass(cls):
-        cls.storage = FileStorage()
-        cls.test_file = "file.json"
-
-    @classmethod
-    def tearDownClass(cls):
-        if os.path.exists(cls.test_file):
-            os.remove(cls.test_file)
-    """
 
     def test_amenity__init__(self):
         new_amenity = Amenity()
