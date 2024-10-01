1b112ad39d004bde1872225b98200c78c1ea45e0
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 19:05:33 -0500

    reverted to using setUpClass and removed file.json

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,9 +8,6 @@ from datetime import datetime
 
 
 class TestUser(unittest.TestCase):
-    """Test cases for the User class"""
-
-    storage = FileStorage()
 
     @classmethod
     def setUpClass(cls):
@@ -20,20 +17,18 @@ class TestUser(unittest.TestCase):
         cls.user.last_name = "Doe"
         cls.user.password = "password123"
         cls.test_file = "file.json"
+        cls.storage = FileStorage()
 
     @classmethod
     def tearDownClass(cls):
+        self.storage.clear()
         if os.path.exists(cls.test_file):
             os.remove(cls.test_file)
 
     def setUp(self):
         self.user = TestUser.user
 
-    def tearDown(self)
-        self.storage.clear()
-
     def test__init__(self):
-        """Test that User initializes with the correct attributes"""
         self.assertIsInstance(self.user, User)
 
     def test_to_dict(self):
