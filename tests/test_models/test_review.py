b4a238bec6d6e7cb1aeca9a29e00a3a6ff54acb7
chepeniv <jose.olmos@atlasschool.com>
2024-09-30 19:57:05 -0500

    simplified and refactored

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,36 +2,15 @@
 
 import unittest
 from models.review import Review
-from models.engine.file_storage import FileStorage
-import os
-
 
 class TestReview(unittest.TestCase):
 
-    storage = FileStorage()
-    
-    @classmethod
-    def setUpClass(cls):
-        cls.review = Review()
-        cls.test_file = "file.json"
-        
-    @classmethod
-    def tearDownClass(cls):
-        if os.path.exists(cls.test_file):
-            os.remove(cls.test_file)
-            
-    def test_initialization(self):
-        self.assertEqual(self.review.place_id, "")
-        self.assertEqual(self.review.user_id, "")
-        self.assertEqual(self.review.text, "")
-        
-    def test_to_dict(self):
-        review_dict = self.review.to_dict()
-        self.assertEqual(review_dict['place_id'], "")
-        self.assertEqual(review_dict['user_id'].strip(), "")
-        self.assertEqual(review_dict['text'], "")
-        self.assertEqual(review_dict['__class__'], "Review")
-        
+    def test_review__init__(self):
+        new_rev = Review()
+        self.assertEqual(new_rev.place_id, "")
+        self.assertEqual(new_rev.user_id, "")
+        self.assertEqual(new_rev.text, "")
+
 if __name__ == "__main__":
     unittest.main()
         
